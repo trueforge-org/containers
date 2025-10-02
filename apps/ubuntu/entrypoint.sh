8ba127d2752355f96c20efb92bd550aaa8d554e8
Kjeld Schouten <info@kjeldschouten.nl>
2025-10-02 14:13:56 +0200

    update ubuntu

--- apps/ubuntu/entrypoint.sh
+++ apps/ubuntu/entrypoint.sh
@@ -2,9 +2,32 @@
 
 source /header.sh
 
+# Execute the CMD arguments
+exec "$@"
+
+# Check if /start.sh exists
+if [ -e /pre-start.sh ]; then
+    if [ -x /pre-start.sh ]; then
+        echo "Info: /start.sh exists, starting application using start.sh"
+        exec /start.sh
+    else
+        echo "Warning: /start.sh exists but is not executable" >&2
+    fi
+fi
+
 # Check if /start.sh exists
 if [ -e /start.sh ]; then
     if [ -x /start.sh ]; then
+        echo "Info: /start.sh exists, starting application using start.sh"
+        exec /start.sh
+    else
+        echo "Warning: /start.sh exists but is not executable" >&2
+    fi
+fi
+
+# Check if /start.sh exists
+if [ -e /post-start.sh ]; then
+    if [ -x /post-start.sh ]; then
         echo "Info: /start.sh exists, starting application using start.sh"
         exec /start.sh "$@"
     else
@@ -12,5 +35,3 @@ if [ -e /start.sh ]; then
     fi
 fi
 
-# Execute the CMD arguments
-exec "$@"
