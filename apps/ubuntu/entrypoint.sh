5523d3cec68da3d11b1af23f51e0477043b028d9
Kjeld Schouten <info@kjeldschouten.nl>
2025-10-02 16:56:47 +0200

    fix some ubuntu related issues

--- apps/ubuntu/entrypoint.sh
+++ apps/ubuntu/entrypoint.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
-source /header.sh
+echo "
+Welcome to a TrueForge ContainerForge container,
+You are entering the vicinity of an area adjacent to a location.
+The kind of place where there might be a monster, or some kind of weird mirror.
+These are just examples; it could also be something much better.
+* Repository: https://github.com/trueforge-org/containerforge
+* Docs: https://truecharts.org
+* Bugs or feature requests should be opened in an GH issue
+* Questions should be discussed in Discord
+"
 
-# Execute the CMD arguments
-exec "$@"
-
-# Check if /start.sh exists
+# Check if /pre-start.sh exists
 if [ -e /pre-start.sh ]; then
     if [ -x /pre-start.sh ]; then
-        echo "Info: /start.sh exists, starting application using start.sh"
-        exec /start.sh
+        echo "Info: /pre-start.sh exists, running pre-start.sh"
+        bash /pre-start.sh
     else
-        echo "Warning: /start.sh exists but is not executable" >&2
+        echo "Warning: /pre-start.sh exists but is not executable" >&2
     fi
 fi
 
@@ -25,13 +31,4 @@ if [ -e /start.sh ]; then
     fi
 fi
 
-# Check if /start.sh exists
-if [ -e /post-start.sh ]; then
-    if [ -x /post-start.sh ]; then
-        echo "Info: /start.sh exists, starting application using start.sh"
-        exec /start.sh "$@"
-    else
-        echo "Warning: /start.sh exists but is not executable" >&2
-    fi
-fi
-
+exec /start.sh "$@"
