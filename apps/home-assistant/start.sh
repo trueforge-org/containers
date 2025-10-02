844cbdb115328c8ec66b380a848063fc14010727
Kjeld Schouten <info@kjeldschouten.nl>
2025-10-03 01:43:41 +0200

    try fix for HA

--- apps/home-assistant/start.sh
+++ apps/home-assistant/start.sh
@@ -6,7 +6,7 @@ mkdir -p "${VENV_FOLDER}"
 uv venv --system-site-packages --link-mode=copy --allow-existing "${VENV_FOLDER}"
 source "${VENV_FOLDER}/bin/activate"
 
-site_packages=$(python -c "import sysconfig; print(sysconfig.get_path('purelib'))")
+site_packages=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
 uv pip install --no-index --find-links="${site_packages}" uv
 
 ln -sf /proc/self/fd/1 /config/home-assistant.log
