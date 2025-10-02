9331750641f48db3376dd401c99a09d6056075b4
Kjeld Schouten <info@kjeldschouten.nl>
2025-10-03 00:55:46 +0200

    tweak tests more

--- .github/actions/app-tests/run.sh
+++ .github/actions/app-tests/run.sh
@@ -13,14 +13,19 @@ if [[ -x "$(command -v container-structure-test)" ]]; then
     echo "Running CST..."
     container-structure-test test --image "${IMAGE}" --pull --config "./apps/${APP}/tests.yaml"
 elif [[ -x "$(command -v goss)" && -x "$(command -v dgoss)" ]]; then
-    echo "Running GOSS.."
-    export GOSS_FILE="./apps/${APP}/tests.yaml"
-    export GOSS_OPTS="--retry-timeout 60s --sleep 1s"
+    if [ -f "$GOSS_FILE" ]; then
+        echo "Running GOSS.."
+        export GOSS_FILE="./apps/${APP}/tests.yaml"
+        export GOSS_OPTS="--retry-timeout 60s --sleep 1s"
     dgoss run "${IMAGE}" || { docker logs "${CONTAINER_ID}"; exit 1; }
+    else
+        echo "GOSS file not found for app ${APP}, skipping GOSS tests."
+    fi
 else
     echo "No testing tool found. Exiting."
+    docker logs "${CONTAINER_ID}"
     exit 1
 fi
 
 echo "Container Logs:"
-
+docker logs "${CONTAINER_ID}"
