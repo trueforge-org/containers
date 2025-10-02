7a7837317671b2fad2604f1e0f96e3d83d3a0e94
Kjeld Schouten <info@kjeldschouten.nl>
2025-10-03 00:52:19 +0200

    more test accuracy

--- .github/actions/app-tests/run.sh
+++ .github/actions/app-tests/run.sh
@@ -7,6 +7,8 @@ IMAGE="${2:?}"
 echo "Starting Container for Log Fetching..."
 CONTAINER_ID=$(docker run -d --name tmp_test_container "${IMAGE}")
 
+sleep 5
+
 if [[ -x "$(command -v container-structure-test)" ]]; then
     echo "Running CST..."
     container-structure-test test --image "${IMAGE}" --pull --config "./apps/${APP}/tests.yaml"
@@ -14,11 +16,11 @@ elif [[ -x "$(command -v goss)" && -x "$(command -v dgoss)" ]]; then
     echo "Running GOSS.."
     export GOSS_FILE="./apps/${APP}/tests.yaml"
     export GOSS_OPTS="--retry-timeout 60s --sleep 1s"
-    dgoss run "${IMAGE}"
+    dgoss run "${IMAGE}" || { docker logs "${CONTAINER_ID}"; exit 1; }
 else
     echo "No testing tool found. Exiting."
     exit 1
 fi
 
 echo "Container Logs:"
-docker logs "${CONTAINER_ID}"
+
