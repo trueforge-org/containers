de22f60fde2189a5acb10583977a83634f6b53c0
Kjeld Schouten <info@kjeldschouten.nl>
2025-10-03 21:32:00 +0200

    even more work on tests

--- apps/lvm-disk-watcher/container_test.go
+++ apps/lvm-disk-watcher/container_test.go
@@ -22,7 +22,7 @@ func Test(t *testing.T) {
 
 	app, err := testcontainers.Run(
 		ctx, image,
-		testcontainers.WithCmdArgs("test", "-f", "/etc/os-release"),
+		testcontainers.WithCmdArgs("test", "-f", "/app/watch_lvm.sh"),
 	)
 	testcontainers.CleanupContainer(t, app)
 	require.NoError(t, err)
