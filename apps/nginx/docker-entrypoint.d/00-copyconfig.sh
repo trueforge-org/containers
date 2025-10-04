0edf1a0cb7b7c72dfc6e4e1f1900aa943b76f956
Kjeld Schouten <info@kjeldschouten.nl>
2025-10-04 22:18:20 +0200

    folder reference bad

--- apps/nginx/docker-entrypoint.d/00-copyconfig.sh
+++ apps/nginx/docker-entrypoint.d/00-copyconfig.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo "[00-copyconfig] Copying default NGINX config files"
-mkdir -p /config/conf.d
+mkdir -p /config/sites
 cp -n /defaults/nginx.conf  /config/nginx.conf
 cp -n /defaults/default.conf /config/sites/default.conf
