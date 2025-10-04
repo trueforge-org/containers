2414d43b38e078130c714fe98fc24e7fdd39b5a1
Kjeld Schouten <info@kjeldschouten.nl>
2025-10-04 19:52:35 +0200

    attempt fixing HA (#53)
    * attempt fixing HA
    
    * double python 3 refs
    
    * undo python change

--- apps/home-assistant/start.sh
+++ apps/home-assistant/start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -euo pipefail
 
 unset UV_SYSTEM_PYTHON
 
@@ -6,12 +7,6 @@ mkdir -p "${VENV_FOLDER}"
 uv venv --system-site-packages --link-mode=copy --allow-existing "${VENV_FOLDER}"
 source "${VENV_FOLDER}/bin/activate"
 
-site_packages=$(python -c "from distutils.sysconfig import get_python_lib; print(get_python_lib())")
-uv pip install --no-index --find-links="${site_packages}" uv
-
 ln -sf /proc/self/fd/1 /config/home-assistant.log
 
-exec \
-    python3 -m homeassistant \
-        --config /config \
-        "$@"
+exec python3 -m homeassistant --config /config "$@"
